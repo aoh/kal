306b0f8b7562eead6ad75b6e06a98fc5e4eed081
Aki Helin <aki.helin@iki.fi>
2016-02-24 19:34:52 +0100

    added some recurring date processing and other stuff

--- kal.scm
+++ kal.scm
@@ -7,6 +7,23 @@
 
 (define version "0.1")
 
+;; date = (tuple 'date d m y wday week)
+
+(define (step date)
+   (lets 
+      ((_ d m y wd w date)
+       (d m y (next-date d m y)))
+      (if (eq? wd 7)
+         (tuple 'date d m y 1 (+ w 1))
+         (tuple 'date d m y (+ wd 1) w))))
+
+(define (day-of x) (ref x 2))
+(define (month-of x) (ref x 3))
+(define (year-of x) (ref x 4))
+
+(define (date? x) 
+   (and (tuple? x) (eq? (ref x 1) 'date)))
+
 (define (check-week-info date)
    (lets 
       ((_ d m y wday week date)
@@ -23,7 +40,6 @@
          (else 
             (tuple 'date d m y wday week)))))
 
-
 ;;;
 
 (define (whitespace? x)
@@ -34,6 +50,11 @@
 
 (define maybe-whitespace
    (get-greedy* (get-byte-if whitespace?)))
+
+(define ws-newline
+   (let-parses
+      ((skip (get-greedy* (get-imm #\space))))
+      42))
 
 (define (ascii-digit? x)
    (and (<= #\0 x) (<= x #\9)))
@@ -57,11 +78,15 @@
          (tuple-case info
             ((week-day n)
                (if (and wday (not (eq? wday n)))
-                  #false
+                  (begin
+                     (print-to stderr "wrong week day")
+                     #false)
                   (tuple 'date d m y n week)))
             ((week n)
                (if (and week (not (eq? week n)))
-                  #false
+                  (begin
+                     (print-to stderr "wrong week number")
+                     #false)
                   (tuple 'date d m y wday n)))
             (else #false)))
       #false))
@@ -79,6 +104,13 @@
       ((skip maybe-whitespace)
        (day
           (get-any-of
+            (get-word "maanantai" 1)
+            (get-word "tiistai" 2)
+            (get-word "keskiviikko" 3)
+            (get-word "torstai" 4)
+            (get-word "perjantai" 5)
+            (get-word "lauantai" 6)
+            (get-word "sunnuntai" 7)
             (get-word "ma" 1)
             (get-word "ti" 2)
             (get-word "ke" 3)
@@ -90,13 +122,14 @@
 
 (define get-week-info
    (get-either
-      get-day-name 
-      get-week-number))
+      get-week-number
+      get-day-name))
       
 (define get-date-info
    (let-parses
       ((d get-date)
-       (is (get-greedy* get-week-info)))
+       (is (get-greedy* get-week-info))
+       (skip ws-newline))
       (check-week-info
          (fold add-week-info d is))))
 
@@ -106,28 +139,50 @@
        (skip (get-imm #\newline)))
       (list->string rs)))
 
-(define get-event
+(define (get-event date)
    (let-parses
       ((skip (get-greedy+ (get-imm #\space)))
        (skip (get-imm #\-))
        (skip (get-imm #\space))
        (line get-line))
-      (tuple 'event line)))
+      (cons date line)))
+
+
+;; a simple case at first
+(define get-recurring 
+   (let-parses
+      ((skip maybe-whitespace)
+       (skip (get-word "every" 42))
+       (skip maybe-whitespace)
+       (skip (get-word "year" 'year))
+       (skip maybe-whitespace)
+       (skip (get-either (get-word "at" 0) (get-word "on" 0)))
+       (skip maybe-whitespace)
+       (d get-nat) (skip (get-imm #\.))
+       (m get-nat) (skip (get-imm #\.))
+       (skip maybe-whitespace)
+       (skip (get-imm #\:))
+       (skip maybe-whitespace)
+       (evt get-line))
+      (list (tuple 'recurring (tuple 'yearly d m) evt))))
 
 (define get-day
    (let-parses
       ((skip maybe-whitespace)
        (d get-date-info)
        (skip (get-imm #\newline))
-       (es (get-greedy* get-event)))
-      (tuple 'day d es)))
+       (es (get-greedy* (get-event d))))
+      es))
 
 (define kal-grammar
    (let-parses
-      ((vals
-         (get-greedy* get-day))
+      ((days
+         (get-greedy*
+            (get-any-of
+               get-recurring
+               get-day)))
        (tail maybe-whitespace))
-      vals))
+      (foldr append null days)))
 
 (define (kal-parse path)
    (let ((data (file->list path)))
@@ -152,8 +207,112 @@
    (print usage-text)
    (print (format-rules command-line-rules)))
 
-(define (kal-write x)
-   (write x))
+
+;; any way to order first by year, then month. then day
+(define (date->scalar d)
+   (if (and d (eq? (ref d 1) 'date))
+      (lets ((_ d m y w1 d2 d))
+         (+ d (* m 100) (* y 100000)))
+      9999999999999999))
+
+(define (date< a b)  
+   (< (date->scalar a) (date->scalar b)))
+
+(define (date<= a b)  
+   (<= (date->scalar a) (date->scalar b)))
+
+
+;; ((date . evt) ...)
+(define (sort-events evs)
+   (sort 
+      (lambda (a b) (date< (car a) (car b)))
+      evs))
+
+(define (print-date date)
+   (lets ((_ d m y wd w date))
+      (print d "." m "." y " " (ref day-names-fi wd) " week " w)))
+
+;; fixed-ish for now
+(define (print-recurring rec)
+   (lets ((_ when evt rec))
+      (print "every year on " (ref when 2) "." (ref when 3) ".: " evt)))
+
+(define (date-of x)
+   (lets 
+      ((d m y H M S (date x))
+       (cweek cwday (week-info d m y)))
+      (tuple 'date d m y cwday cweek)))
+
+(define (match-date? date rec)
+   (tuple-case rec
+      ((yearly d m)
+         (and (= m (month-of date)) (= d (day-of date))))
+      (else
+         (error "match-date: what recurrence is " rec))))
+
+(define (cons-happening-on date)
+   (lambda (evs recurring)
+      ;(print "Checking if " recurring " happens on " date)
+      (if (match-date? date (ref recurring 2))
+         (cons (cons date (ref recurring 3)) evs)
+         evs)))
+
+(define (add-recurrences-on date recs tail)
+   (fold (cons-happening-on date) tail recs))
+
+(define (recurring-events date last recs)
+   (if (date< last date)
+      null
+      (add-recurrences-on date recs
+         (recurring-events (step date) last recs))))
+
+(define (kal-output x dict)
+   (lets
+      ((start-time (get dict 'date (time)))
+       (end-time (+ start-time (* 2 week)))
+       (now (date-of start-time))
+       (end (date-of end-time))
+       (evs 
+         (keep 
+            (lambda (x) 
+               (and 
+                  (date<= now (car x))
+                  (date<= (car x) end)))
+            (keep pair? x)))
+       (recs
+         (remove pair? x))
+       (evs 
+         (sort-events 
+            (append 
+               (recurring-events now end recs)
+               evs))))
+
+      (fold
+         (lambda (date evt)
+            (if (not (equal? date (car evt)))
+               (begin
+                  (if date (print ""))
+                  (print-date (car evt))))
+            (display " - ")
+            (print (cdr evt))
+            (car evt))
+         #false evs)
+
+   (let ((recurs (remove pair? x)))
+      (if (pair? recurs)
+         (begin
+            (print "")
+            (for-each print-recurring recurs))))))
+
+(define (kal-read-files paths)
+   (fold
+      (lambda (cals file)
+         (let ((this (kal-parse file)))
+            ;(print "this is " this)
+            (if (and this cals)
+               (append cals this)
+               #false)))
+      null paths))
 
 (define (kal dict args)
    (cond
@@ -164,17 +323,13 @@
          (print "dict: " dict)
          (print "args: " args)
          0)
+      ((kal-read-files args) =>
+         (lambda (evs)
+            (kal-output evs dict)
+            0))
       (else
-         (map
-            (lambda (x)
-               (print x)
-               (kal-write (kal-parse x))
-               (print ""))
-            args)
-         0)))
+         1)))
 
-(
 (lambda (args)
    (process-arguments (cdr args) command-line-rules usage-text kal))
-   (list "kal" "test.txt"))
 
